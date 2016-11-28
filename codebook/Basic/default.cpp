b4b54da3de6d9819c33c18bb584581ef15c4035c
Si-Chen Lin <ss8651twtw@gmail.com>
2016-11-28 06:27:31 +0000

    add default

---
+++ codebook/Basic/default.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define IOS ios_base::sync_with_stdio(0);cin.tie(0);
+#define MP make_pair
+#define PB push_back
+#define FF first
+#define SS second
+
+typedef long long LL;
+typedef pair<int, int> PII;
+
+const int MOD = 1000000007;
+const double PI = 3.1415926535897932384626433832795;
+const int dx[] = {-1, 0, 1, 0};
+const int dy[] = {0, 1, 0, -1};
