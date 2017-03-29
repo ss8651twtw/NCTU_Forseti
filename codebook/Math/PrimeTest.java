a6f03d4be0aeca35a9cef4781dac888973336b36
ss8651twtw <ss8651twtw@gmail.com>
2017-03-30 01:52:25 +0800

    add Prime-Test

---
+++ codebook/Math/PrimeTest.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+import java.util.*;
+public class PrimeTest {
+   public static void main(String[] args) {
+      Scanner in = new Scanner(System.in);
+      BigInteger n = in.nextBigInteger();
+      in.close();
+      System.out.println(n.isProbablePrime(10) ? "prime" : "not prime");
+   }
+}
